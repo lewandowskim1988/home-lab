0dc2007be2089ac5576a203bdcb4af7071ecfdc3
Michał Lewandowski <michal.lewandowski1988@gmail.com>
2025-04-09 08:51:36 +0200

    add init script

--- init.sh
+++ init.sh
@@ -1,31 +1,51 @@
-#!/usr/bin/env sh
-
+#!/usr/bin/env bash
 set -euo pipefail
 
 # Update and upgrade system
 sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
 
-# Install rocm 6.2.1 for AMD GPU support (this version was latest working with RX 7600 XT)
+# https://rocm.docs.amd.com/projects/install-on-linux/en/docs-6.2.1/install/prerequisites.html
 sudo apt install "linux-headers-$(uname -r)" "linux-modules-extra-$(uname -r)"
 sudo apt install python3-setuptools python3-wheel
-sudo usermod -a -G render,video $LOGNAME # Add the current user to the render and video groups
-wget https://repo.radeon.com/amdgpu-install/6.2.1/ubuntu/noble/amdgpu-install_6.2.60201-1_all.deb
-sudo apt install ./amdgpu-install_6.2.60201-1_all.deb
-sudo apt update
-sudo apt install amdgpu-dkms rocm
 
-# Needed after rocm install
+# https://rocm.docs.amd.com/projects/install-on-linux/en/docs-6.2.1/install/native-install/ubuntu.html
+sudo mkdir --parents --mode=0755 /etc/apt/keyrings
+wget https://repo.radeon.com/rocm/rocm.gpg.key -O - | \
+  gpg --dearmor | sudo tee /etc/apt/keyrings/rocm.gpg > /dev/null
+echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/rocm.gpg] https://repo.radeon.com/amdgpu/6.2.1/ubuntu noble main" \
+  | sudo tee /etc/apt/sources.list.d/amdgpu.list
+sudo apt update
+echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/rocm.gpg] https://repo.radeon.com/rocm/apt/6.2.1 noble main" \
+  | sudo tee --append /etc/apt/sources.list.d/rocm.list
+echo -e 'Package: *\nPin: release o=repo.radeon.com\nPin-Priority: 600' \
+  | sudo tee /etc/apt/preferences.d/rocm-pin-600
+sudo apt update
+
+sudo apt install -y amdgpu-dkms
 sudo reboot
 
+sudo apt install -y rocm
+
+# https://rocm.docs.amd.com/projects/install-on-linux/en/docs-6.2.1/install/post-install.html
 sudo tee --append /etc/ld.so.conf.d/rocm.conf <<EOF
 /opt/rocm/lib
 /opt/rocm/lib64
 EOF
 sudo ldconfig
-export PATH=$PATH:/opt/rocm-6.2.1/bin
 
 # Install wireguard for remote access
-sudo apt install wireguard
+sudo apt install -y wireguard cron iptables
+
+systemctl start wg-quick@wg0.service
+systemctl enable wg-quick@wg0.service
+
+sudo sysctl -w net.ipv4.ip_forward=1
+
+sudo iptables -t nat -A POSTROUTING -s 10.66.66.0/24 -o enp112s0 -j MASQUERADE
+
+# Prepare disk for OpenEBS
+sudo pvcreate /dev/nvme0n1p4
+sudo vgcreate openebs /dev/nvme0n1p4
 
 curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="server" sh -s - --disable traefik
 
